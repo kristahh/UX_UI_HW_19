77de633a9d2c45ee92ac1e5e143fe5495f4405dc
kristahh <104269969+kristahh@users.noreply.github.com>
2022-05-18 14:57:27 -0700

    fix-buttons

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -1,2 +1,15 @@
-$('#text').hide().text('Some Text Animation').fadeIn(300);
-<input data-anijs="if: click, on: .blue, do: zoomIn, to: .front"></input>
+// $('#text').hide().text('Some Text Animation').fadeIn(300);
+// <input data-anijs="if: click, on: .blue, do: zoomIn, to: .front"></input>
+
+
+
+// If you want to delay every link on your page, you can do it with jQuery like this
+
+$(function(){
+    $("a").click(function(evt){
+        var link = $(this).attr("href");
+        setTimeout(function() {
+            window.location.href = link;
+        }, 500);
+    });
+});
